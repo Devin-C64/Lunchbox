a4d66e5c03c1572927fd782aab035d83625da16f
jmoneill15 <124748245+jmoneill15@users.noreply.github.com>
2024-12-02 04:50:34 +0000

    Created a page for the offers section, so the page design will be available to be linked when we eventually add the button to said page. I am trying out a different push attempt since my android studio is being weird with git auth credentials.

---
+++ app/src/main/java/com/example/lunchbox/ui/offers/OffersFragment.kt
@@ -0,0 +1,42 @@
+package com.example.lunchbox.ui.offers
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.lunchbox.databinding.FragmentOffersBinding
+
+class OffersFragment : Fragment() {
+
+    private var _binding: FragmentOffersBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val offersViewModel =
+            ViewModelProvider(this)[OffersViewModel::class.java]
+
+        _binding = FragmentOffersBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textOffers
+        offersViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
