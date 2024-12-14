4cea07059624f7234288c500e43976e9b1e761f7
Abbey Liu <Abbey.Liu@temple.edu>
2024-12-14 13:44:30 -0500

    Adding if/else stmt that shows a new post after pressing "post item"

--- app/src/main/java/com/example/lunchbox/ui/add/AddFragment.kt
+++ app/src/main/java/com/example/lunchbox/ui/add/AddFragment.kt
@@ -28,10 +28,64 @@ import androidx.lifecycle.findViewTreeLifecycleOwner
 import androidx.navigation.findNavController
 import com.example.lunchbox.R
 import com.example.lunchbox.databinding.FragmentAddBinding
+import com.example.lunchbox.databinding.FragmentOffersCompletedBinding
 import com.example.lunchbox.ui.home.HomeFragment
+import com.example.lunchbox.ui.profile.ProfilePage
 import java.io.File
 import java.time.Month
 
+class AddFragment : Fragment() {
+    private lateinit var binding: FragmentAddBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAddBinding.inflate(inflater, container, false)
+
+        // Toast for Save Draft
+        binding.saveDraftbtn.setOnClickListener {
+            Toast.makeText(requireContext(), "Draft Saved", Toast.LENGTH_SHORT).show()
+        }
+
+        // Toast for Post Item
+        binding.postItemBtn.setOnClickListener {
+            Toast.makeText(requireContext(), "Item Posted", Toast.LENGTH_SHORT).show()
+            // Bundle data to send to Profile fragment
+            val bundle = Bundle()
+            bundle.putInt("postedItem", 1)
+
+            it.findNavController().navigate(R.id.navigation_profile, bundle)  // Navigating to 'Profile' page
+//
+//            val newFrag = HomeFragment()
+//            val fragManager = requireActivity().supportFragmentManager
+//            val transaction = fragManager.beginTransaction()
+//            transaction.replace(R.id.navigation_add, newFrag).commit()
+        }
+
+        // Camera Functionality
+        val cameraButton = binding.captureImgBtn
+        val previewView = binding.previewView
+
+        val launcher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val image = result.data?.extras?.get("data") as Bitmap
+                previewView.setImageBitmap(image)
+            }
+        }
+
+        cameraButton.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            launcher.launch(cameraIntent)
+        }
+
+        return binding.root
+    }
+}
+/*
 class AddFragment : Fragment() {
 
     private var _binding: FragmentAddBinding? = null
@@ -61,10 +115,12 @@ class AddFragment : Fragment() {
         // Toast for Post Item
         binding.postItemBtn.setOnClickListener {
             Toast.makeText(requireContext(), "Item Posted", Toast.LENGTH_SHORT).show()
-            val newFrag = HomeFragment()
-            val fragManager = requireActivity().supportFragmentManager
-            val transaction = fragManager.beginTransaction()
-            transaction.replace(R.id.navigation_add, newFrag).commit()
+            it.findNavController().navigate(R.id.action_offers_to_sent)  // Navigating to 'Sent' page
+//
+//            val newFrag = HomeFragment()
+//            val fragManager = requireActivity().supportFragmentManager
+//            val transaction = fragManager.beginTransaction()
+//            transaction.replace(R.id.navigation_add, newFrag).commit()
         }
 
         // Camera Functionality
@@ -93,6 +149,8 @@ class AddFragment : Fragment() {
         _binding = null
     }
 }
+
+*/
 
 /*
 class AddFragment : Fragment() {
--- app/src/main/java/com/example/lunchbox/ui/profile/ProfilePage.kt
+++ app/src/main/java/com/example/lunchbox/ui/profile/ProfilePage.kt
@@ -2,14 +2,20 @@ package com.example.lunchbox.ui.profile
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.example.lunchbox.MainActivity
 import com.example.lunchbox.R
 
 import com.example.lunchbox.databinding.FragmentProfilePageBinding
+import com.example.lunchbox.ui.add.AddFragment
 
 class ProfilePage :Fragment() {
+
+    var postedItem = 0
 
     private lateinit var binding: FragmentProfilePageBinding
     override fun onCreateView(
@@ -21,6 +27,21 @@ class ProfilePage :Fragment() {
 
         binding.imageButton2.setOnClickListener{
             it.findNavController().navigate(R.id.action_navigation_profile_to_settingsFragment)
+        }
+
+        // get data from AddFragment
+        val args = this.arguments
+        val inputData = args?.getInt("postedItem")
+        if (inputData != null) {
+            postedItem = inputData
+        }
+
+        if (postedItem == 1){
+            binding.newPost.visibility = VISIBLE
+            binding.editTextText5.visibility = GONE
+        } else{
+            binding.newPost.visibility = GONE
+            binding.editTextText5.visibility = VISIBLE
         }
 
         return binding.root
